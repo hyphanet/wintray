c1261d70c7e81be06e038e24713d2a96c4e3ec12
Stephen Oliver <steve@infincia.com>
2017-07-11 14:28:03 -0400

    Add support for finding 64-bit Opera browser

--- Browsers/Opera.cs
+++ Browsers/Opera.cs
@@ -10,6 +10,51 @@ namespace FreenetTray.Browsers
         private readonly string _path;
         private readonly bool _isInstalled;
 
+        private static string OperaRegistryKey = @"Software\Opera Software\Last Stable Install Path";
+
+
+        private static string RegistryPathForView(RegistryView view) {
+            RegistryKey hive = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, view);
+            RegistryKey key = hive.OpenSubKey(OperaRegistryKey);
+            if (key == null) {
+                return null;
+            }
+
+            string value = key.GetValue("opera.exe") as string;
+
+            return value;
+        }
+
+        // find the path to opera, preferring 64-bit if available
+        private static string RegistryPath {
+            get {
+                if (Environment.Is64BitOperatingSystem) {
+                    var path64 = RegistryPathForView(RegistryView.Registry64);
+
+                    if (path64 != null) {
+                        return path64;
+                    }
+
+                    var path32 = RegistryPathForView(RegistryView.Registry32);
+
+                    if (path32 != null) {
+                        return path32;
+                    }
+
+                    return null;
+                }
+                else {
+                    var path32 = RegistryPathForView(RegistryView.Registry32);
+
+                    if (path32 != null) {
+                        return path32;
+                    }
+
+                    return null;
+                }
+            }
+        }
+
         public Opera()
         {
             /*
@@ -17,7 +62,8 @@ namespace FreenetTray.Browsers
              * on what it supports in its place, if anything, has not been forthcoming.
              */
             // Key present with Opera 21.
-            var possiblePath = (string)Registry.GetValue(@"HKEY_CURRENT_USER\Software\Opera Software", "Last Stable Install Path", null) + "opera.exe";
+            var possiblePath = RegistryPath;
+
             _isInstalled = File.Exists(possiblePath);
             if (_isInstalled)
             {

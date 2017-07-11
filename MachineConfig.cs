4d3a1a66c388bdef4de8d88e634ba1c6fd2a15f4
Stephen Oliver <steve@infincia.com>
2017-07-11 13:53:27 -0400

    Add class for finding the best available JRE
    Attempts to find a 64-bit JRE if the OS is 64-bit, falls back to 32-bit
    if a 32-bit JRE is available
    
    Should work regardless of whether the tray process is 32-bit or 64-bit

---
+++ MachineConfig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace FreenetTray {
+    public enum JREType {
+        None,
+        JRE64Bit,
+        JRE32Bit,
+    }
+
+    public class MissingJRE: Exception { }
+
+    public class MachineConfig {
+
+        private static string JRERegistryKey = @"Software\\JavaSoft\\Java Runtime Environment";
+
+        private static bool Has64BitJRE {
+            get {
+                RegistryKey local64 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                RegistryKey jreKey64 = local64.OpenSubKey(JRERegistryKey);
+                if (jreKey64 != null) {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        private static bool Has32BitJRE {
+            get {
+                RegistryKey local32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+                RegistryKey jreKey32 = local32.OpenSubKey(JRERegistryKey);
+                if (jreKey32 != null) {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        // find the best available JRE
+        public static JREType GetBestAvailableJRE {
+            get {
+                if (Environment.Is64BitOperatingSystem) {
+                    // if we're on a 64-bit OS and have a 64-bit JRE we want to use it
+                    if (Has64BitJRE) {
+                        FNLog.Debug("64-bit JRE found");
+                        return JREType.JRE64Bit;
+                    }
+                }
+
+                // if we're on 32-bit OS or no 64-bit JRE was found, we have no other choice
+                // but to try the 32-bit JRE
+                if (Has32BitJRE) {
+                    FNLog.Debug("32-bit JRE found");
+
+                    return JREType.JRE32Bit;
+                }
+
+                // no JRE found, this could happen in practice if the only available JRE was uninstalled
+                FNLog.Debug("No JRE found");
+
+                return JREType.None;
+            }
+        }
+    }
+}
